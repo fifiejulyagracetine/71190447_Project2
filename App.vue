e93223f57f038eaa8e76da52039dc1f2b70a59bc
fifiejulyagracetine <90020148+fifiejulyagracetine@users.noreply.github.com>
2022-12-13 16:33:51 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+      <input v-model="username" placeholder="masukkan username..."/>
+      <button v-on:click="masuk()">Masuk</button>
+  </div>
+</template>
+
+<script>
+import {db} from './db'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      username:'',
+    }
+  },
+  methods:{
+    masuk(){
+      db.collection('user').add({username: this.username});
+      this.username = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top:200px;
+  margin-bottom: 250px;
+}
+
+
+</style>
